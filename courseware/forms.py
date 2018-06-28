f348d41dd631904c365fcbad45ce2db1fc659e9e
sickkjedi <protos3@gmail.com>
2018-06-28 18:16:39 +0200

    Added Elements and Chapters

--- courseware/forms.py
+++ courseware/forms.py
@@ -1,9 +1,10 @@
-from django.forms import ModelForm, ModelMultipleChoiceField
+from django.forms import ModelForm, ModelMultipleChoiceField, CharField
 
 from Elearn.settings import BASE_DIR
 from loginUser.models import MyUser
-from courseware.models import Groups, Courses
+from courseware.models import Groups, Courses, HtmlTE, Chapters, TeachingElementBase
 from django.contrib.admin.widgets import FilteredSelectMultiple
+from tinymce.widgets import TinyMCE
 import os
 
 
@@ -12,8 +13,9 @@ class GroupEditForm(ModelForm):
                                         required=False,
                                         widget=FilteredSelectMultiple(
                                             verbose_name='User',
-                                            is_stacked=False ),
+                                            is_stacked=False),
                                      label='')
+
     class Meta:
         model = Groups
         fields = ['group_name', 'description', 'year', 'users']
@@ -30,8 +32,9 @@ class CourseEditForm(ModelForm):
                                         required=False,
                                         widget=FilteredSelectMultiple(
                                             verbose_name='Groups',
-                                            is_stacked=False ),
+                                            is_stacked=False),
                                      label='')
+
     class Meta:
         model = Courses
         fields = ['course_name', 'description', 'teacher', 'groups']
@@ -41,3 +44,30 @@ class CourseEditForm(ModelForm):
         css = {
             'all': (os.path.join(BASE_DIR, '/static/admin/css/widgets.css'),),
         }
+
+
+class ChapterEditForm(ModelForm):
+    elements = ModelMultipleChoiceField(queryset=TeachingElementBase.objects.all(),
+                                        required=False,
+                                        widget=FilteredSelectMultiple(
+                                            verbose_name='User',
+                                            is_stacked=False),
+                                     label='')
+
+    class Meta:
+        model = Chapters
+        fields = ['name', 'elements']
+
+    class Media:
+        js = ['/admin/jsi18n/']
+        css = {
+            'all': (os.path.join(BASE_DIR, '/static/admin/css/widgets.css'),),
+        }
+
+
+class HtmlTEForm(ModelForm):
+    html = CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 50, 'class': 'form-control'}))
+
+    class Meta:
+        model = HtmlTE
+        fields = ['name', 'description', 'html']
--- courseware/urls.py
+++ courseware/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.views import i18n
 from django.conf.urls import url
 
@@ -8,8 +8,20 @@ urlpatterns = [
     path('groups', views.ListGroups.as_view(), name='groups'),
     path('groups/add_group', views.AddGroup.as_view(), name='add_group'),
     path('groups/edit_group/<int:pk>', views.EditGroup.as_view(), name='edit_group'),
+
     path('courses', views.ListCourses.as_view(), name='courses'),
     path('courses/add_course', views.AddCourse.as_view(), name='add_course'),
     path('courses/edit_course/<int:pk>', views.EditCourse.as_view(), name='edit_course'),
+
+    path('elements', views.ListElements.as_view(), name='elements'),
+    path('elements/html', views.ListHtmlElements.as_view(), name='html_elements'),
+    path('elements/add_html', views.AddHtmlElement.as_view(), name='add_html_element'),
+    path('elements/reflection', views.ListReflectionElements.as_view(), name='reflection_elements'),
+    path('elements/add_reflection', views.AddReflectionElement.as_view(), name='add_reflection_element'),
+
+    path('chapters', views.ListChapters.as_view(), name='chapters'),
+    path('chapters/add_chapter', views.AddChapter.as_view(), name='add_chapter'),
+    path('chapters/edit_chapter/<int:pk>', views.EditChapter.as_view(), name='edit_chapter'),
+
     url(r'^admin/jsi18n/$', i18n.null_javascript_catalog, name='jsi18n'),
 ]
