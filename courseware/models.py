16a0442f61933c38b0ed85dd63775cf29a476ecd
sickkjedi <protos3@gmail.com>
2018-07-18 13:47:13 +0200

    Chapter ordering implemented

--- courseware/models.py
+++ courseware/models.py
@@ -50,7 +50,21 @@ class Reflection(TeachingElementBase):
 
 class Chapters(models.Model):
     name = models.CharField(max_length=255)
-    course = models.ForeignKey("Courses", on_delete=models.CASCADE)
+    course = models.ForeignKey("Courses", on_delete=models.CASCADE, default=None)
+    order_id = models.IntegerField(default=1)
+
+    def save(self, *args, **kwargs):
+        # This means that the model isn't saved to the database yet
+        if self._state.adding:
+            # Get the maximum display_id value from the database
+            last_id = self.__class__.objects.all().aggregate(largest=models.Max('order_id'))['largest']
+
+            # aggregate can return None! Check it first.
+            # If it isn't none, just use the last ID specified (which should be the greatest) and add one to it
+            if last_id is not None:
+                self.order_id = last_id + 1
+
+        super(Chapters, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
